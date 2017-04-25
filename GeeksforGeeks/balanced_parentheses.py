b07af316c9b62cb8ece8ef2b49ac2ca75edd3b9f
lforbes <lauraforbes92@gmail.com>
2017-04-25 22:56:37 +0100

    Create balanced_parentheses.py

---
+++ GeeksforGeeks/balanced_parentheses.py
@@ -0,0 +1,51 @@
+"""
+From http://www.geeksforgeeks.org/check-for-balanced-parentheses-in-an-expression/
+Also https://www.hackerrank.com/challenges/balanced-brackets
+Given an expression string exp, examine whether the pairs and the orders of "{","}","(",")","[","]" are correct in exp.
+For example, the program should print 'balanced' for exp = "[()]{}{[()()]()}" and 'not balanced' for exp = "[(])"
+"""
+
+
+def is_balanced(exp):
+    """
+    Description:
+        Checks if given string contains balanced parentheses.
+    Args:
+        exp (str): String to check.
+    Returns:
+         str: "balanced" or "not balanced"
+    """
+    # List of opening parentheses
+    opening = ['{', '(', '[']
+    # List of closing parentheses
+    closing = ['}', ')', ']']
+
+    # Keep a record of opening parentheses to match with closing parentheses
+    checklist = []
+
+    for char in exp:
+        if char in opening:
+            checklist.append(char)
+        else:
+            # Only check for closing parentheses if list is not empty
+            if not checklist:
+                return "not balanced"
+
+            # Find position of closing char in closing list
+            closing_char = closing.index(char)
+            # Check if corresponding char in opening list matches opening char to be removed from checklist
+            if opening[closing_char] == checklist[-1]:
+                # Remove matched parentheses
+                del checklist[-1]
+            else:
+                return "not balanced"
+
+    if not checklist:
+        # If no chars left in checklist
+        return "balanced"
+    return "not balanced"
+
+# Get the number of strings to check
+num_of_strs = int(raw_input().strip())
+for _ in range(num_of_strs):
+    print is_balanced(raw_input().strip())
